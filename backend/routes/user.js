e2c1e206353681fe98da2f466b23738719a4bee0
Thea Blachon <thea.blachon73@gmail.com>
2023-01-26 15:24:06 +0100

    :bug:correction du lien vers le js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 export default router.get("/", (req, res) => {
     res.json({
-        firstName: "Alex",
-        lastName: "Zerah",
-        email: "alex.zerah@gmail.com"
+        firstName: "Touille",
+        lastName: "Arthur",
+        email: "juju.lucie@gmail.com"
     });
 });
